fe21e04dfb5888fd0bb2f4eb98ce7659b583eb50
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-28 17:08:02 +0100

    logica completata

--- Palindroma/main.js
+++ Palindroma/main.js
@@ -1,5 +1,5 @@
 /*Prima acquisisco la parola tramite il prompt poi:
-    1. acqusisco la parola con un prompt;
+    1. acquisisco la parola con un prompt;
     2. con split divido la stringa in tante sotto stringhe quante le lettere;
     3. con reverse ne inverto l'ordine;
     4. con join le unisco in una sola stringa;
@@ -9,11 +9,17 @@
 
 
 function palindromo(messaggio) {
-
-    let parola = prompt(messaggio).replaceAll(" ","").toUpperCase();
+   //inizializzo le variabili
     let revParola;
-    revParola = parola.split("").reverse().join("");
     let esito;
+
+    //acquisisco la stringa, in cui elimino gli spazi e la trasformo tutti i caratteri in maiuscolo
+    let parola = prompt(messaggio).replaceAll(" ","").toUpperCase();
+
+   //divido la parola o le parole in caratteri, ne inverto l'ordine e poi li riunisco
+    revParola = parola.split("").reverse().join("");
+    
+    //confronto le due stringhe
     if (revParola === parola){
 
         esito = alert("è un palindromo");
